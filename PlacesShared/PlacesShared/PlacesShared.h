0bcf896a859d4e0bead25c939632c3c301cb4aaa
sesur <sergiusuru@ymail.com>
2024-07-10 23:15:06 +0300

    Task - Create PlaceShared framework

---
+++ PlacesShared/PlacesShared/PlacesShared.h
@@ -0,0 +1,18 @@
+//
+//  PlacesShared.h
+//  PlacesShared
+//
+//  Created by Sergiu on 10.07.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlacesShared.
+FOUNDATION_EXPORT double PlacesSharedVersionNumber;
+
+//! Project version string for PlacesShared.
+FOUNDATION_EXPORT const unsigned char PlacesSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlacesShared/PublicHeader.h>
+
+
